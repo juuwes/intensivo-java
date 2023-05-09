e4a4cd30030a313e557008315f42bbddf2dc0cd7
juuwes <contatosilvajuliana@gmail.com>
2023-05-09 15:55:33 -0300

    Project Created

---
+++ src/main/java/com/devsuperior/intensivojava/IntensivojavaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.intensivojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivojavaApplication.class, args);
+	}
+
+}
