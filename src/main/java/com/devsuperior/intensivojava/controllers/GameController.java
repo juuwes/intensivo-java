25924ff1c880291c30a49e54cafc66365ecf6018
juuwes <contatosilvajuliana@gmail.com>
2023-05-10 18:28:55 -0300

    GET Game By Id

--- src/main/java/com/devsuperior/intensivojava/controllers/GameController.java
+++ src/main/java/com/devsuperior/intensivojava/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.intensivojava.dto.GameDTO;
 import com.devsuperior.intensivojava.dto.GameMinDTO;
 import com.devsuperior.intensivojava.services.GameService;
 
@@ -20,7 +22,16 @@ public class GameController {
 // injetar Service no Controller
 	@Autowired
 	private GameService gameService;
-
+	
+//ENDPOINTS:
+// configurar o verbo HTTP com argumento.
+	@GetMapping(value = "/{id}")
+// @PathVariable: corresponder com id na requisição.
+	public GameDTO findAll(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;		
+	}
+	
 // @GetMapping: mapear Service com o verbo HTTP | endopoint buscar/obter os objetos - metodo GET
 	@GetMapping
 	public List<GameMinDTO> findAll() {
--- src/main/java/com/devsuperior/intensivojava/services/GameService.java
+++ src/main/java/com/devsuperior/intensivojava/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.intensivojava.dto.GameDTO;
 import com.devsuperior.intensivojava.dto.GameMinDTO;
 import com.devsuperior.intensivojava.entities.Game;
 import com.devsuperior.intensivojava.repositories.GameRepository;
@@ -18,13 +20,29 @@ public class GameService {
 	@Autowired // Spring resolve a dependencia
 	private GameRepository gameRepository; // injetando uma instancia
 	
-// Customizar a saida na API
-// Service devolve DTO - Arquitetura
+/* boa pratica | todo metodo do service para ser transacional, principios das transações
+ *  ACID (atomica, consistente, isolada e duravel - notação : @Transactional (import SPRING)
+ *  (readOnly = true) : não irá ter escrita no bd (+ rapido)*/
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		
+/* findById() : recebe um id e retorna um objeto.
+* findById() do repository retorna tipo Optional, para buscar/pegar o game dentro do Optional .get()
+* tratamento de exceção pode ser feito (id inexistente). */
+		Game result = gameRepository.findById(id).get();
+		
+// buscar do banco de dados, converter DTO
+		return new GameDTO(result);
+	}
+	
+/* Customizar a saida na API
+* Service devolve DTO - Arquitetura */
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() { // findAll() buscar todos.
 		List<Game> result = gameRepository.findAll();
 
-//lib do java | stream(): permite fazer operações com sequencias de dados, operação map(): transforma objetos
-// para voltar um stream() para lista .toList()
+/* lib do java | stream(): permite fazer operações com sequencias de dados, operação map(): transforma objetos
+* para voltar um stream() para lista .toList()*/
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 
