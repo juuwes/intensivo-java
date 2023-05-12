57a33dacf4acf3d1a3e20d891f3dd2f2cf10355f
juuwes <contatosilvajuliana@gmail.com>
2023-05-12 16:54:12 -0300

    Move games on list

--- src/main/java/com/devsuperior/intensivojava/repositories/GameListRepository.java
+++ src/main/java/com/devsuperior/intensivojava/repositories/GameListRepository.java
@@ -1,9 +1,17 @@
 package com.devsuperior.intensivojava.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.intensivojava.entities.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+	// Quando existe uma consulta SQL, sem a função SELECT, mas com outros comandos (ex. UPDATE), colocar @Modifying
+	@Modifying
+	@Query(nativeQuery = true, 
+	value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 
 }
--- src/main/java/com/devsuperior/intensivojava/services/GameListService.java
+++ src/main/java/com/devsuperior/intensivojava/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.intensivojava.dto.GameListDTO;
 import com.devsuperior.intensivojava.entities.GameList;
+import com.devsuperior.intensivojava.projections.GameMinProjection;
 import com.devsuperior.intensivojava.repositories.GameListRepository;
+import com.devsuperior.intensivojava.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,10 +18,35 @@ public class GameListService {
 	@Autowired 	
 	private GameListRepository gameListRepository; 
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() { 
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+	
+	@Transactional
+	//mover jogos/atualizar jogos	
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		//remover item de uma posição:
+		GameMinProjection obj = list.remove(sourceIndex);
+		// adicionar item a lista.
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i <= max; i++) {
+			// list.get(i).getId(): busca id do jogo
+			// i = nova posição --- "lista 2, id 9, posição i
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+
 	}
 
 }
